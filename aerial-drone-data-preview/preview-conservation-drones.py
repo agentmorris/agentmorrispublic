17b58767888a029327f515e6a6a76115ebf8b61b
Dan Morris <agentmorris@gmail.com>
2023-03-31 18:58:41 -0700

    Added rendering script for aerial seabirds west africa

--- aerial-drone-data-preview/preview-conservation-drones.py
+++ aerial-drone-data-preview/preview-conservation-drones.py
@@ -113,6 +113,20 @@ print('Average elephant box width: {}'.format(np.mean(elephant_box_widths)))
 i_video = 2
 i_frame = 4
 
+# species: -1: unknown, 0: human, 1: elephant, 2: lion, 3: giraffe, 4: dog, 5: crocodile, 6: hippo, 
+# 7: zebra, 8: rhino. 3 and 4 occur only in real data. 5, 6, 7, 8 occur only in synthetic data.
+category_id_to_name = {
+    -1: 'unknown', 
+    0: 'human', 
+    1: 'elephant', 
+    2: 'lion', 
+    3: 'giraffe', 
+    4: 'dog', 
+    5: 'crocodile', 
+    6: 'hippo', 
+    7: 'zebra', 
+    8: 'rhino'}
+
 video_info = all_annotations[i_video]
 frame_annotations = [ann for ann in video_info['annotations'] if ann['frame_num'] == i_frame]
 
@@ -134,8 +148,6 @@ assert frame_image_path is not None
 
 from visualization import visualization_utils as visutils
 
-category_id_to_name = {0:'animal',1:'person'}
-
 detection_formatted_boxes = []
 
 pil_im = visutils.open_image(frame_image_path)
@@ -145,8 +157,8 @@ image_h = pil_im.size[1]
 # frame_ann = frame_annotations[0]
 for frame_ann in frame_annotations:
     det = {}
-    det['conf'] = 1.0
-    det['category'] = frame_ann['class']    
+    det['conf'] = None
+    det['category'] = frame_ann['species']    
     box = [frame_ann['left']/image_w,
            frame_ann['top']/image_h,
            frame_ann['w']/image_w,
@@ -157,11 +169,8 @@ for frame_ann in frame_annotations:
     
 output_file = r'g:\temp\conservation_drones_sample_image.jpg'
 visutils.draw_bounding_boxes_on_file(frame_image_path, output_file, detection_formatted_boxes,       
-                                     confidence_threshold=0.0)
+                                     confidence_threshold=0.0,detector_label_map=category_id_to_name)
 
 
-    
-
-            
-            
-        
+import shutil
+shutil.copyfile(frame_image_path,r'g:\temp\conservation_drones_sample_image_unannotated.jpg')
