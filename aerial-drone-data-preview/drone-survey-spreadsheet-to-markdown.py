0711b335adb160834d5402e02df8e450eec3eb0d
Dan Morris <agentmorris@gmail.com>
2023-04-11 19:46:42 -0700

    rc0 of drone/aerial dataset list

--- aerial-drone-data-preview/drone-survey-spreadsheet-to-markdown.py
+++ aerial-drone-data-preview/drone-survey-spreadsheet-to-markdown.py
@@ -17,7 +17,7 @@ df = pd.read_csv(input_file)
 
 output_lines = []
 
-starting_header_level = '#'
+starting_header_level = '###'
 
 # i_row = 0; row = df.iloc[i_row]
 for i_row,row in df.iterrows():
@@ -27,26 +27,60 @@ for i_row,row in df.iterrows():
         break
     
     s = ''
-    s += starting_header_level + ' ' + row['Name'] + '\n'
+    s += starting_header_level + ' ' + row['Name'] + '\n\n'
+    
     s += row['Short description'] + '\n'
+    
+    s += '  \n'
+    
+    if isinstance(row['Citation'],str):
+        s += row['Citation'] + '\n\n'
         
-    s += starting_header_level + '#' + ' Download and format information' + '\n'
+    # s += '<a href="{}">{}</a>\n'.format(row['URL'],row['URL'])
     
     size_bytes = 1000*1000*1000*float(row['Size in GB'])
     size_string = humanfriendly.format_size(size_bytes)
     
-    s += '{}, downloadable via {} from {}\n'.format(size_string, row['Hosting site'],row['Download mechanism'])
+    s += '* {}, downloadable via {} from {} (<a href="{}">download link</a>)\n'.format(
+        size_string, row['Download mechanism'], row['Hosting site'], row['URL'])
     
     metadata_addendum = ''
     if isinstance(row['Metadata standard'],str):
         metadata_addendum = ' (' + row['Metadata standard'] + ')'
-    s += 'Metadata in {} format{}\n'.format(row['Metadata format'],metadata_addendum)
+    s += '* Metadata in {} format{}\n'.format(row['Metadata raw format'],metadata_addendum)
+    
+    s += '* Categories: {}\n'.format(row['Categories/species'])
+    s += '* Vehicle type: {}\n'.format(row['Vehicle type'])
+    
+    image_addendum = ''
+    if isinstance(row['Image notes'],str):
+        note = row['Image notes']
+        note = note[0].lower() + note[1:]
+        image_addendum = ' (' + note + ')'
+        
+    s += '* Image information: {} {} images{}\n'.format(row['Number of images or videos'],row['Channels'],image_addendum)
+    
+    s += '* Annotation information: {} {}\n'.format(int(row['Number of annotations']),row['Annotation type'])
+    s += '* Typical animal size in pixels: {}\n'.format(row['Typical animal width in pixels'])
+    
+    if isinstance(row['Sample code'],str):
+        
+        code_url = row['Sample code']
+        code_link_name = code_url.split('/')[-1]
+        
+        s += '* Code to render sample annotated image: <a href="{}">{}</a>\n'.format(
+            row['Sample code'],code_link_name)
+    
+    s += '  \n'
+    s += '  \n'
     
     s += '<img src="{}" width=700>\n'.format(row['Sample image'])
     
-    output_lines.append(s)
+    s += '  \n'
+    s += '  \n'
     
-        
+    output_lines.append(s)
+
 with open(output_file,'w') as f:
     for s in output_lines:
         f.write(s)
