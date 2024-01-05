5c0d880075f60865818f1abb877e5cd3f9d43235
Dan Morris <agentmorris@gmail.com>
2024-01-05 11:53:45 -0800

    added bioacoustics model zoo test driver

---
+++ misc/bioacoustics-model-zoo-test.py
@@ -0,0 +1,116 @@
+#%% Constants and imports
+
+"""
+# Installation on Windows
+#
+# Python 3.10 is the latest version that's compatible with TF 2.10, which is the 
+# last version with native GPU windows support.
+#
+# https://www.tensorflow.org/install/pip#windows-native
+
+mamba create -n opensoundscape python=3.10 pip
+mamba activate opensoundscape
+
+# This was enough for TF to report the GPU as being prsent, but when I actually tried
+# to run inference, I got:
+#
+# libdevice not found at ./libdevice.10.bc
+#
+# ...until I *also* installed the system CUDA 11.2 from:
+#
+# https://developer.nvidia.com/cuda-11.2.0-download-archive
+mamba install -c conda-forge cudatoolkit=11.2 cudnn=8.1.0
+pip install "tensorflow<2.11"
+pip install opensoundscape
+
+# OpenSoundscape forces installation of a newer version of protobuf, which makes older 
+# versions of TF sad, but for this test, we're not using features of OpenSoundscape that 
+# require it, so force installation of 3.19.6 again.
+pip install protobuf==3.19.6
+pip install tensorflow_hub
+mamba install -c conda-forge spyder
+
+python -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))"
+"""
+
+import os
+import pandas as pd
+import numpy as np
+import torch
+import tensorflow as tf
+
+# Just testing installation, this is not imported directly
+#
+# pip install opensoundscape
+import opensoundscape # noqa
+
+# Just testing installation, this is not imported directly
+#
+# pip install tensorflow tensorflow_hub
+import tensorflow_hub # noqa
+
+data_folder = r'G:\temp\perch-stuff\audiomoth\audiomoth'
+assert os.path.isdir(data_folder)
+
+
+#%% Create model
+
+model = torch.hub.load('kitzeslab/bioacoustics-model-zoo', 'Perch')
+
+
+#%% Enumerate wave files
+
+wav_files = os.listdir(data_folder)
+wav_files = [fn for fn in wav_files if fn.lower().endswith('.wav')]
+wav_files = [os.path.join(data_folder,fn) for fn in wav_files]
+
+print('Enumerated {} data files'.format(len(wav_files)))
+
+
+#%% Run inference
+
+print("GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+# i_file = 0; fn = wav_files[i_file]
+for i_file,fn in enumerate(wav_files):
+    
+    print('Running inference for file {} of {}: {}'.format(
+        i_file,len(wav_files),fn))
+    predictions = model.predict([fn]) 
+    output_file = os.path.splitext(fn)[0] + '.csv'
+    print('Writing results to {}'.format(output_file))
+    predictions.to_csv(output_file,header=True,index=True,float_format='%.5g')
+    # embeddings = model.generate_embeddings([fn])
+    
+
+#%% Find detections above a threshold
+
+csv_files = os.listdir(data_folder)
+csv_files = [fn for fn in csv_files if fn.lower().endswith('.csv')]
+csv_files = [os.path.join(data_folder,fn) for fn in csv_files]
+
+print('Enumerated {} csv files'.format(len(csv_files)))
+
+metadata_columns = ['file', 'start_time', 'end_time']
+
+# i_file = 0; fn = csv_files[i_file]
+for i_file,fn in enumerate(csv_files):
+    
+    print('Analyzing results in {}'.format(fn))
+
+    df = pd.read_csv(fn)
+    
+    # Convert from log-p to p
+    for c in df.columns:
+        if c not in metadata_columns:
+            df[c] = df[c].apply(np.exp)
+            
+    threshold = 0.1
+    
+    # row = df.iloc[0]
+    for i_row,row in df.iterrows():
+        for c in df.columns:
+            if c not in metadata_columns:
+                if row[c] > threshold:
+                    t = row['start_time']
+                    print('{} at {} ({})'.format(c,t,row[c]))
